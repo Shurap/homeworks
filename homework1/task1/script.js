995cce1094666173dd930eca22beb469a42bc98a
Shurap <bap.essp@mail.ru>
2019-05-25 15:04:56 +0300

    finished homework1

--- homework1/task1/script.js
+++ homework1/task1/script.js
@@ -1,21 +1,17 @@
 const userName = prompt('Enter youre name:');
 
-function renamed(userName) {
+function transformName(userName) {
   if (userName.search(/[0-9]/) === -1) {
-    return newName = userName.split('').reverse().join('');
+    return userName.split('').reverse().join('');
   } else {
       const newName = userName.split('').map((element, index) => {
-      if (index % 2) {
-        return element.toUpperCase();
-      } else {
-        return element;
-      }
+        return index % 2 ? element.toUpperCase() : element;
     });
     return newName.join('');
   }    
 }
 
-alert(renamed(userName));
+alert(transformName(userName));
 
 
   
---
+++ homework1/task2/script.js
@@ -0,0 +1,11 @@
+function makeNewPage() {
+  document.body.innerHTML = '<section>\
+                              <h1>New Block</h1>\
+                              <h2>Homework 1</h2>\
+                              <h2>Task 2</h2>\
+                            </section>'
+}
+
+function goToOtherPage() {
+  location.href='http://google.com';
+}
